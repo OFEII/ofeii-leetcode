4ebb993cdfc762707e26fd28671903bd65804191
OFEII <2282252066@qq.com>
2020-03-18 23:18:36 +0800

    +daily question 16

---
+++ DailyQuestion/16.js
@@ -0,0 +1,15 @@
+var compressString = function(S) {
+
+    let res = "", i = j = 0 
+    while (j < S.length - 1) {
+        if (S[j] !== S[j + 1]) {
+            res += S[i] + (j+1 - i)
+            i = j+1
+        }
+        j++
+    }
+
+    res += S[i] + (j - i + 1)
+    return res.length < S.length ? res : S
+};
+双指针模拟，使用两个下标 i 和 j，枚举头指针i，移动尾指针j
